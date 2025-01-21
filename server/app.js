8c300c7aee1d5234c5813871d39ffecd77416b5d
Ajay <ajaykumawat9904@gmail.com>
2025-01-21 09:44:30 +0530

    added nodemon and middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 const app = express();
 const port = 5001;
 
 import { router } from "./routes/index.js";
 
-app.use('/', router);
+// Middleware
+app.use(json({ limit: '50mb' }));
+app.use(urlencoded({ limit: '50mb', extended: true }));
+
+// Routes
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
