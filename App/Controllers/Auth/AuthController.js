95b1345cc6edfbdf4df258c82f3f0504ce2849c3
Ajay <ajaykumawat9904@gmail.com>
2025-02-05 10:27:37 +0530

    feat(user): created forgot password route

--- App/Controllers/Auth/AuthController.js
+++ App/Controllers/Auth/AuthController.js
@@ -66,8 +66,6 @@ const verifyOtpAndCreateUser = async (req, res) => {
   try {
     const { firstName, email, password, otp } = req.body;
     const storedOtp = req.cookies.otp;
-    console.log(storedOtp, "storedOtp");
-    console.log(otp, "otp");
 
     if (!storedOtp || storedOtp !== otp) {
       return sendResponse(
@@ -182,9 +180,106 @@ const resendOtp = async (req, res) => {
   }
 };
 
+const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return sendResponse(
+        res,
+        {},
+        "User not found",
+        RESPONSE_FAILURE,
+        RESPONSE_CODE.NOT_FOUND
+      );
+    }
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    res.cookie("otp", otp, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 5 * 60 * 1000, // 5 minutes
+    });
+
+    // Email template path
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const emailTemplatePath = path.join(
+      __dirname,
+      "../../Common/email_template/forgot_password_email_template.html"
+    );
+
+    await sendMail(email, user.firstName, otp, emailTemplatePath);
+
+    return sendResponse(
+      res,
+      {},
+      "OTP sent successfully",
+      RESPONSE_SUCCESS,
+      RESPONSE_CODE.SUCCESS
+    );
+  } catch (error) {
+    console.error(`UserController.forgotPassword() -> Error: ${error}`);
+  }
+};
+
+const verifyOtp = async (req, res) => {
+  try {
+    const { otp } = req.body;
+    const storedOtp = req.cookies.otp;
+
+    if (!storedOtp || storedOtp !== otp) {
+      return sendResponse(
+        res,
+        {},
+        "Invalid or expired OTP",
+        RESPONSE_FAILURE,
+        RESPONSE_CODE.UNAUTHORISED
+      );
+    }
+
+    res.clearCookie("otp"); // Remove OTP after successful verification
+
+    return sendResponse(
+      res,
+      {},
+      "OTP verified successfully",
+      RESPONSE_SUCCESS,
+      RESPONSE_CODE.SUCCESS
+    );
+  } catch (error) {
+    console.error(`UserController.verifyOtp() -> Error: ${error}`);
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.findOneAndUpdate(
+      { email: email },
+      { password: hashedPassword }
+    );
+
+    return sendResponse(
+      res,
+      {},
+      "Password reset successfully",
+      RESPONSE_SUCCESS,
+      RESPONSE_CODE.SUCCESS
+    );
+  } catch (error) {
+    console.error(`UserController.resetPassword() -> Error: ${error}`);
+  }
+};
+
 export default {
   create,
   verifyOtpAndCreateUser,
   login,
   resendOtp,
+  forgotPassword,
+  verifyOtp,
+  resetPassword,
 };
--- App/Controllers/Auth/_route.js
+++ App/Controllers/Auth/_route.js
@@ -7,6 +7,9 @@ route.post('/signup', AuthController.create);
 route.post('/login', AuthController.login);
 route.post('/verify-otp', AuthController.verifyOtpAndCreateUser);
 route.post('/resend-otp', AuthController.resendOtp);
+route.post('/forgot-password', AuthController.forgotPassword);
+route.post('/verify-forgotPassword-otp', AuthController.verifyOtp);
+route.post('/reset-password', AuthController.resetPassword);
 
 export default route;
 
