71c22ad62a50b9faabd4e45839d2d4b70eefa753
Ajay <ajaykumawat9904@gmail.com>
2025-02-21 09:54:11 +0530

    feat(service): created service add route

--- App/Controllers/_router.js
+++ App/Controllers/_router.js
@@ -1,11 +1,13 @@
 import express from "express";
 import Auth from "./Auth/_route.js";
 import User from "./User/_route.js";
+import Service from "./Service/_route.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/auth", Auth);
-app.use('/user', User);
+app.use("/user", User);
+app.use("/service", Service);
 
 export default app;
