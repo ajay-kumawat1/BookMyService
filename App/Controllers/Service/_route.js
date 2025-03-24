bfe947aaa04f646214b6bcee9a8982652f6c92aa
Ajay <ajaykumawat9904@gmail.com>
2025-03-24 15:00:26 +0530

    fix(service): remove auth-validation get-all service route

--- App/Controllers/Service/_route.js
+++ App/Controllers/Service/_route.js
@@ -7,7 +7,7 @@ const route = Router();
 route.post("/create", validJWTNeeded, serviceController.create);
 route.get("/getMy", validJWTNeeded, serviceController.getMy);
 route.get("/get/:id", validJWTNeeded, serviceController.getById);
-route.get("/getAll", validJWTNeeded, serviceController.getAll);
+route.get("/getAll", serviceController.getAll);
 route.put("/bookService/:id", validJWTNeeded, serviceController.bookService);
 route.put("/update/:id", validJWTNeeded, serviceController.update);
 export default route;
